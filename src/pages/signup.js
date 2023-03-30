18602b1b95c0f80fd959de61ac8c0565544e2437
maroeplin <rosemarcel@outlook.de>
2023-03-30 14:55:37 +0200

    adding emoji

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -76,7 +76,10 @@ export default function Signup() {
       </button>
     
       </> ):( 
-        <h1 className="text-2xl text-center">Vielen Dank für die Registrierung. Du erhältst deinen <span className="font-bold">Anmelde-Code mit einer SMS</span> an deine Mobilnummer gesendet.</h1>
+        <div className="pt-24">
+          <h1 className="text-2xl text-center">Vielen Dank für die Registrierung. Du erhältst deinen <span className="font-bold">Anmelde-Code mit einer SMS</span>   <span role="img" aria-label="done">✅</span>an deine Mobilnummer gesendet.</h1>
+        
+        </div>
         )
      }
         
