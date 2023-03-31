41a925fd0ef2b28617c99236a1bef476af36326d
maroeplin <rosemarcel@outlook.de>
2023-03-31 22:24:25 +0200

    fix

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -88,8 +88,8 @@ export default function Signup() {
         id="phone"
         required
         title="Die Mobilnummer sollte folgendes Schema haben: +49 111 1111 1111"
-        minLength="13"
-        maxLength="14"
+        minLength="11"
+        maxLength="15"
         className="block w-full pt-2 pb-2 pl-2 border border-gray-300 rounded-md shadow-sm focus:border-teal-500 focus:ring-teal-500"
         placeholder="+49 111 1111 1111"
         onChange={(e) => setPhone(e.target.value)}
