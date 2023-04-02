737eebb4b6832c1bf2155027ee30d80effd7d23d
maroeplin <rosemarcel@outlook.de>
2023-04-02 13:25:09 +0200

    minor css fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,7 +100,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="w-auto pt-48 m-auto text-center bg-amber-500">
+        <div className="w-auto pt-48 m-auto text-center ">
           <h1 className="text-3xl font-light md:text-4xl">Herzlich Willkommen!</h1>
 
           {!isAuthenticated ? (
