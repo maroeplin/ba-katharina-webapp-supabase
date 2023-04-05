f914284a03f4052e574b85635faea197fdbdc1af
maroeplin <rosemarcel@outlook.de>
2023-04-05 22:29:00 +0200

    minor fix: changing the mobile number

--- src/utils/twilio.js
+++ src/utils/twilio.js
@@ -5,5 +5,5 @@ let client = new twilio(process.env.NEXT_PUBLIC_SUBAPASE_URL, process.env.NEXT_P
 cronJob = require('cron').CronJob;
 
 let textJob = new cronJob('0 23 * * *', function(){
-    client.messages.create( { to: '+4915221491112', from: '+1 585 595 2499', body: 'Hallo. Bitte rufe die Webapp auf und erstelle einen Eintrag. Danke'}, function(err, data) {});
+    client.messages.create( { to: '+4917630785581', from: '+1 585 595 2499', body: 'Hallo. Bitte rufe die Webapp auf und erstelle einen Eintrag. Danke'}, function(err, data) {});
 }, null, true);
